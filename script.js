60f5f3a2ca2f6282a5cd7c3ff0d7bc439e9e0999
Anderey Prokhorov <beta737@yandex.ru>
2021-12-09 13:03:04 +0300

    Выполнил задание 11 урока

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
+'use strict';
+
+const btn = document.getElementById('btn'),
+    square = document.getElementById('square'),
+    btnSmoll = document.getElementById('e_btn');
+let text = document.getElementById('text'),
+    range = document.getElementById('range'),
+    rangeSpan = document.getElementById('range-span'),
+    circle = document.getElementById('circle');
+    
+console.log(circle);
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    square.style.backgroundColor = text;
+    document.getElementById('text').value = '';
+    btnSmoll.style.display = 'none';
+});
+
+text.addEventListener('input', (e) => {
+    e.preventDefault();
+    text = e.target.value;
+});
 
 
-
+range.addEventListener('input', (e) => {
+    e.preventDefault();
+    rangeSpan.textContent = e.target.value;
+    circle.style.height = rangeSpan.textContent + '%';
+    circle.style.width = rangeSpan.textContent + '%';    
+});
 
 
